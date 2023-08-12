7a51084be45ee219d7b3b8cb1f64fba7a6e60bf3
harsh <harshkhosla9945@gmail.com>
2023-08-12 21:41:16 +0530

    changes on collection

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,8 +1,10 @@
 const { connect } = require("mongoose")
 const  DB_URI= "mongodb+srv://harsh:Harsh9945khosla@cluster0.osfevs6.mongodb.net/test"
-DB_NAME="lms"
+DB_NAME="Harsh"
 const connectDb = async () => {
   return connect(DB_URI, { dbName: DB_NAME })
+
+
 }
 
 module.exports = { connectDb }
